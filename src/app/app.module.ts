b0c7a8f57d7b5f3e6d262f9032d0650aafbc8942
RistaGear <89870535+RistaGear@users.noreply.github.com>
2021-10-27 20:43:21 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//import { Insomnia } from '@ionic-native/insomnia/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
